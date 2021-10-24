f485b22b7db66f24af63588bc26ac4c33df413aa
k-ohmoto <87626886+k-ohmoto@users.noreply.github.com>
2021-10-24 14:10:58 +0900

    Initial commit

---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,57 @@
+// import { useContext } from "react";
+// import { UserContext } from "../../provider/UserProvider";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { userState } from "../../store/userState";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((value) => {
+  return {
+    id: value,
+    name: `じゃけえ${value}`,
+    image: "https://source.unsplash.com/73pyV0JJOmE",
+    email: "23e@faer.com",
+    phone: "090-0000-2222",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  // const { userInfo, setUserInfo } = useContext(UserContext);
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+
+  const onClickSwitch = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
+
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
